ac1b7e3d9c4ef35b89f11abf2cd6acae194d0d5f
Nero <17shou.dev@gmail.com>
2026-08-07 17:25:15 +0700

    initial commit

--- main.cpp
+++ main.cpp
@@ -1,50 +1,18 @@
-#if defined(_DEBUG)
-	#include <stdlib.h>
-	#include <crtdbg.h>
-#endif
-
-#include "GameWindow.h"
-
-
-#ifdef _DEBUG
 #include <iostream>
-static int reportHook(int reportType, char* userMessage, int* ) {
-	if (reportType == _CRT_ASSERT) {
-		std::cout << userMessage << std::endl;
-		return TRUE;
-	}
-	return FALSE;
+
+// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+int main() {
+    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the <b>lang</b> variable name to see how CLion can help you rename it.
+
+    const auto lang = "C++";
+    std::cout << "Hello and welcome to " << lang << "!\n";
+
+    for (int i = 1; i <= 5; i++) {
+        // TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+        std::cout << "i = " << i << std::endl;
+    }
+
+    return 0;
+    // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
 }
-#endif // DEBUG
-
-int WINAPI WinMain(_In_ HINSTANCE , _In_opt_ HINSTANCE , _In_ PSTR , _In_ int )
-{
-
-#if defined(_DEBUG)
-	int tmpFlag = _CrtSetDbgFlag(_CRTDBG_REPORT_FLAG);
-	tmpFlag |= _CRTDBG_LEAK_CHECK_DF;
-	_CrtSetDbgFlag(tmpFlag);
-
-	_CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_DEBUG);
-	_CrtSetReportHook(&reportHook);
-#endif
-	HRESULT hr = CoInitializeEx(nullptr, COINITBASE_MULTITHREADED);
-	if (FAILED(hr))
-	{
-		return false;
-	}
-	GameWindow game;
-	if (game.Initialize())
-	{
-		while (game.IsRunning())
-		{
-			game.Broadcast();
-		}
-	}
-	CoUninitialize();
-#if defined(_DEBUG)
-	_CrtDumpMemoryLeaks();
-#endif
-	return 0;
-}
-
